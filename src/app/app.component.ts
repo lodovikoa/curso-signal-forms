01bfc2ba13799003ed0ccaf9fcb980c9716dac8f
lodovikoa <lodoviko@gmail.com>
2026-08-13 08:40:19 -0300

    feat: formularios com Signal - Inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { form } from '@angular/forms/signals';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class App {
-  protected readonly title = signal('curso-signal-forms');
+  protected formModel = signal({
+    name: '',
+    email: '',
+  })
+
+  form = form(this.formModel)
+
+  constructor() {
+    this.form.email().value()
+  }
 }
